0d600bf5601b57ddb97cf6c5679cc6cb796b2858
FachrulAziz <fachrulaziz95@gmail.com>
2023-03-16 11:41:35 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/FachrulAziz/go-say-hellooo
+module github.com/FachrulAziz/go-say-hellooo/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhellooo
 
-func SayHello() string {
-	return "Hello kak"
+func SayHello(name string) string {
+	return "Hello" + name
 }
